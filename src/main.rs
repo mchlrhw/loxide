86d89250045e65778698d565c640e1c389a14010
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2021-12-27 09:52:05 +0000

    Implement prompt loop and error helpers

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,54 @@
-use std::{env, process};
+use std::{env, io::Write, process};
+
+fn report(line: usize, where_: &str, message: &str) {
+    println!("[line {}] Error{}: {}", line, where_, message);
+}
+
+fn error(line: usize, message: &str) {
+    report(line, "", message);
+}
+
+#[derive(Debug)]
+struct Token;
+
+struct Scanner;
+
+impl Scanner {
+    fn new(input: &str) -> Self {
+        Self
+    }
+
+    fn scan(&mut self) -> Vec<Token> {
+        todo!();
+    }
+}
+
+fn run(input: &str) -> anyhow::Result<()> {
+    let mut scanner = Scanner::new(input);
+
+    for token in scanner.scan() {
+        println!("{:?}", token);
+    }
+
+    Ok(())
+}
 
 fn run_prompt() -> anyhow::Result<()> {
-    todo!()
+    let mut line = String::new();
+
+    loop {
+        print!("> ");
+        std::io::stdout().flush()?;
+
+        std::io::stdin().read_line(&mut line)?;
+        if line.is_empty() {
+            break;
+        }
+
+        run(&line)?;
+    }
+
+    Ok(())
 }
 
 fn run_file(_path: &str) -> anyhow::Result<()> {
