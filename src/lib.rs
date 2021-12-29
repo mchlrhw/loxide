af743e62ea5c6c56eed83c59aa29f6abe5927b02
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2021-12-29 10:43:56 +0000

    Move scanner to its own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,10 @@
+pub mod scanner;
 pub mod token;
+
+fn report(line: usize, where_: &str, message: &str) {
+    println!("[line {}] Error{}: {}", line, where_, message);
+}
+
+pub fn error(line: usize, message: &str) {
+    report(line, "", message);
+}
